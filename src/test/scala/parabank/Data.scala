ebeddaf919e4b06b0b2db2c4fc3e7de352498e39
christian-gomezp <christian.gomezp@udea.edu.co>
2026-04-21 21:45:29 -0500

    Update username in Data object

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -2,7 +2,7 @@ package parabank
 
 object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
-    val username = "christang123"
+    val username = "christang1234"
     val password = "christang123"
 
 }
