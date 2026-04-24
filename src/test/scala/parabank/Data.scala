684a3b22e572feeccca25750da276952380e75d2
christian-gomezp <christian.gomezp@udea.edu.co>
2026-04-23 19:24:24 -0500

    Add account IDs and amount to Data object

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,5 +4,8 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "christang1234"
     val password = "christang123"
+    val fromAccountId = "15120"
+    val toAccountId = "15231"
+    val amount = "1"
 
 }
