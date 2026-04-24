7c1f3c5f7b25a468c77f726669a17ce20bbe8ccc
christian-gomezp <christian.gomezp@udea.edu.co>
2026-04-23 19:35:10 -0500

    Change username and password in Data object
    Updated username and password for Data object.

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -2,8 +2,8 @@ package parabank
 
 object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
-    val username = "john"
-    val password = "demo"
+    val username = "chrisg"
+    val password = "chrisg"
     val fromAccountId = "15120"
     val toAccountId = "15231"
     val amount = "1"
