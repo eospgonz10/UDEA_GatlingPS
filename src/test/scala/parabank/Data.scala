e1807ebb9786243ce2bbf2749244eddb9835ab6e
Estiven Ospina González <149256165+eospgonz10@users.noreply.github.com>
2026-04-25 21:43:16 +0000

    Se corrige limite de peticiones simultaneas y ventana de tiempo

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -13,20 +13,20 @@ object Data{
     // Historia 1: Login
     val loginNormalUsers = 100
     val loginPeakUsers = 200
-    val loginNormalDuration: FiniteDuration = 2.minutes
-    val loginPeakDuration: FiniteDuration = 2.minutes
+    val loginNormalDuration: FiniteDuration = 30.seconds
+    val loginPeakDuration: FiniteDuration = 30.seconds
     val loginP95NormalMs = 2000
     val loginP95PeakMs = 5000
 
     // Historia 2: Transferencias simultaneas
-    val transferStressDuration: FiniteDuration = 3.minutes
-    val transferRampUpDuration: FiniteDuration = 1.minute
+    val transferStressDuration: FiniteDuration = 30.seconds
+    val transferRampUpDuration: FiniteDuration = 15.seconds
     val transferTargetTps = 150
 
     // Historia 3: Estado de cuenta
     val statementAccountId = fromAccountId
     val statementUsers = 200
-    val statementDuration: FiniteDuration = 3.minutes
+    val statementDuration: FiniteDuration = 30.seconds
     val statementP95Ms = 3000
     val statementMaxErrorPercent = 1.0
 
